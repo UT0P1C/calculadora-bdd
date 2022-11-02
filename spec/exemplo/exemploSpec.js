ef68fbd0995d10ca67f0321b1f8db7f8109bae89
UT0P1C <ljonham@gmail.com>
2022-11-01 22:20:34 -0300

    implementando a base do projeto

--- spec/exemplo/exemploSpec.js
+++
@@ -1,9 +0,0 @@
-describe("Deve demonstrar o uso do Jasmine no NodeJs", () => {
-	it("deve demonstrar o primeiro uso" , () => {
-		expect(true).toEqual(true);
-	});
-
-	it("deve demonstrar o uso do karma em modo de escuta", () => {
-		expect(1).toEqual(1);
-	});
-});
