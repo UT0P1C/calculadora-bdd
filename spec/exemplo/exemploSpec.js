b3c44774154f7e7e727d8d486587ba6ed30b76f0
UT0P1C <ljonham@gmail.com>
2022-11-01 21:45:23 -0300

    first commit

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe("Deve demonstrar o uso do Jasmine no NodeJs", () => {
+	it("deve demonstrar o primeiro uso" , () => {
+		expect(true).toEqual(true);
+	});
+
+	it("deve demonstrar o uso do karma em modo de escuta", () => {
+		expect(1).toEqual(1);
+	});
+});
